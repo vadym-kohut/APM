b9b9b124faa38a25401f5196c9496645776fc255
Vadym Kohut <vkohut@sii.pl>
2022-09-28 13:21:27 +0200

    update product added

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -46,7 +46,7 @@ export class ProductListComponent implements OnInit {
   }
 
   productSelected(product: Product): void {
-    this.store.dispatch(ProductActions.setCurrentProduct({ product }));
+    this.store.dispatch(ProductActions.setCurrentProduct({ currentProductId: product.id }));
   }
 
 }
--- src/app/products/state/product.actions.ts
+++ src/app/products/state/product.actions.ts
@@ -7,7 +7,7 @@ export const toggleProductCode = createAction(
 
 export const setCurrentProduct = createAction(
     '[Product] Set Current Product',
-    props<{ product: Product }>()
+    props<{ currentProductId: number }>()
 );
 
 export const clearCurrentProduct = createAction(
@@ -29,5 +29,20 @@ export const loadProductsSuccess = createAction(
 
 export const loadProductsFailure = createAction(
     '[Product] Load Fail',
+    props<{ error: string }>()
+);
+
+export const updateProduct = createAction(
+    '[Product] Update Product',
+    props<{ product: Product }>()
+);
+
+export const updateProductSuccess = createAction(
+    '[Product] Update Product Success',
+    props<{ product: Product }>()
+);
+
+export const updateProductFailure = createAction(
+    '[Product] Update Product Fail',
     props<{ error: string }>()
 );
