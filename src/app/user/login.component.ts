abbaf37da334bacefb1aae087cb220ff8d34ac57
Vadym Kohut <vadym.kohut@gmail.com>
2022-09-28 06:02:48 +0200

    hw3 done

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -6,6 +6,7 @@ import { Store } from '@ngrx/store';
 import { AuthService } from './auth.service';
 import { getMaskUserName } from './state/user.reducer';
 import * as UserActions from './state/user.actions';
+import { Observable } from 'rxjs';
 
 @Component({
   templateUrl: './login.component.html',
@@ -13,16 +14,14 @@ import * as UserActions from './state/user.actions';
 })
 export class LoginComponent implements OnInit {
   pageTitle = 'Log In';
-  maskUserName: boolean;
   showUserName: boolean;
+  maskUserName$: Observable<boolean>;
 
   constructor(private store: Store<any>, private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
     // TODO: Unsubscribe
-    this.store.select(getMaskUserName).subscribe(
-      maskUserName => this.maskUserName = maskUserName
-    );
+    this.maskUserName$ = this.store.select(getMaskUserName);
   }
 
   cancel(): void {
